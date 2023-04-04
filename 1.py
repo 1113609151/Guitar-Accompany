e8ccc3d2d35ca43e05f193c94e3d30dcb37ef20b
1113609151 <1113609151@qq.com>
2023-04-04 15:25:43 +0800

    1

--- 1.py
+++ 1.py
@@ -1,3 +1,8 @@
-print('⭐')
-with open("./1.txt", "w") as f:
-        f.write("⭐")
+# 开始计时，每计时1s中将总时长进行输出
+import time
+#开始计时
+start_time = time.time()
+while(1):
+        time.sleep(1)
+        print(f"总时长：{time.time() - start_time}")
+                
